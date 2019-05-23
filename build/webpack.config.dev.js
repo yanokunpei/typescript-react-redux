07a379bbd9371cd6023c8c1072fe5afaf6127a4f
yanokunpei <ninian138@gmail.com>
2019-05-23 17:24:15 +0900

    [fix] precommit hook

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -3,37 +3,38 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.common.js');
 const path = require('path');
 
-module.exports = (env, argv) => merge(common(env, argv), {
-  mode: 'development',
-  output: {
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
-              sourceMap: true,
+module.exports = (env, argv) =>
+  merge(common(env, argv), {
+    mode: 'development',
+    output: {
+      publicPath: '/dist/',
+      filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                sourceMap: true,
+              },
             },
-          },
-          'postcss-loader',
-        ],
-      },
-    ],
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './static'),
-    open: true,
-    openPage: 'dist/',
-    historyApiFallback: true,
-  },
-});
+            'postcss-loader',
+          ],
+        },
+      ],
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, './static'),
+      open: true,
+      openPage: 'dist/',
+      historyApiFallback: true,
+    },
+  });
--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -10,62 +10,63 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const mode = 'production';
 const outDir = '../dist';
 
-module.exports = (env, argv) => merge(common(env, argv), {
-  mode: mode,
-  output: {
-    path: path.resolve(__dirname, outDir),
-    filename: `assets/[name].[hash].js`,
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-      }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
-    splitChunks: {
-      name: true,
-      maxInitialRequests: 4,
-      cacheGroups: {
-        react: {
-          test: /[\\/]node_modules[\\/]react/,
-          name: 'react',
-          chunks: 'all',
-        },
-        babel: {
-          test: /[\\/]node_modules[\\/]@babel/,
-          name: 'polyfill',
-          chunks: 'all',
+module.exports = (env, argv) =>
+  merge(common(env, argv), {
+    mode: mode,
+    output: {
+      path: path.resolve(__dirname, outDir),
+      filename: `assets/[name].[hash].js`,
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+        }),
+        new OptimizeCSSAssetsPlugin({}),
+      ],
+      splitChunks: {
+        name: true,
+        maxInitialRequests: 4,
+        cacheGroups: {
+          react: {
+            test: /[\\/]node_modules[\\/]react/,
+            name: 'react',
+            chunks: 'all',
+          },
+          babel: {
+            test: /[\\/]node_modules[\\/]@babel/,
+            name: 'polyfill',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[hash:base64]',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[hash:base64]',
+              },
             },
-          },
-          'postcss-loader',
-        ],
-      },
+            'postcss-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'assets/style.[hash].css',
+        chunkFilename: 'assets/style.[id].[hash].css',
+      }),
+      new CleanWebpackPlugin(),
+      new BundleAnalyzerPlugin(),
     ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'assets/style.[hash].css',
-      chunkFilename: 'assets/style.[id].[hash].css',
-    }),
-    new CleanWebpackPlugin(),
-    new BundleAnalyzerPlugin(),
-  ],
-});
+  });
